fcf185831bd0cadbb6efcead7c2b0138d72165af
r.alimoradi <alimoradifaranam@gmail.com>
2024-08-31 15:45:21 +0330

    add user command

--- JWTSampleProject/Controllers/AccountController.cs
+++ JWTSampleProject/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using JWTSampleProject.CQRS.InputModel;
 using JWTSampleProject.Models;
+using KarafariniPlans.Core.Services.Commands.GeneralData;
 using MediatR;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,23 @@ namespace JWTSampleProject.Controllers
                 data = await _mediator.Send(inputModel),
                 StatusCode = true
             });
+
+
+        /// <summary>
+        /// افزودن کاربر
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost("AddUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> AddUser([FromBody] AddUserCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+
 
         /// <summary>
         /// لاگین
