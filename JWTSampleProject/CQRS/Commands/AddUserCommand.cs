8eaf8f6119d823e2a8601c6feb59ba3215a1f334
r.alimoradi <alimoradifaranam@gmail.com>
2024-08-31 15:10:24 +0330

    add remove update add to entity user

---
+++ JWTSampleProject/CQRS/Commands/AddUserCommand.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using JWTSampleProject.Context;
+using JWTSampleProject.Models;
+using MediatR;
+using System.Data;
+
+namespace KarafariniPlans.Core.Services.Commands.GeneralData
+{
+    public class AddUserCommandHandler : IRequestHandler<AddUserCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAppDbContext _context;
+
+        public AddUserCommandHandler(IMapper mapper, IAppDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Handle(AddUserCommand request, CancellationToken cancellationToken)
+        {
+
+            //var product = _mapper.Map<Product>(request);
+
+            var obj = new User
+            {
+                Email = request.Email,
+                Id = request.Id,
+                IsActive = request.IsActive,
+                Phone = request.Phone,
+                PassWord = request.PassWord,
+                Role = request.Role,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                BirthDate = request.BirthDate
+            };
+
+            await _context.Users.AddAsync(obj);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+
+    public class AddUserCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+        public string Email { get; set; }
+        public string PassWord { get; set; }
+        public string Role { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ JWTSampleProject/CQRS/Commands/RemoveUserCommand.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using JWTSampleProject.Context;
+using MediatR;
+using System.Net;
+using System.Web.Http;
+
+namespace KarafariniPlans.Core.Services.Commands.GeneralData
+{
+    public class RemoveUserCommandHandler : IRequestHandler<RemoveUserCommand>
+    {
+        private readonly IAppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public RemoveUserCommandHandler(IAppDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(RemoveUserCommand request, CancellationToken cancellationToken)
+        {
+            var obj = _appDbContext.Users.Find(request.Id);
+
+            if (obj != null)
+            {
+                _appDbContext.Products.Remove(obj);
+                await _appDbContext.SaveChangesAsync();
+
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+
+    public class RemoveUserCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
