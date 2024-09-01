03df5aad7bd6e3287cd47697c6c53c30fd7a838a
r.alimoradi <alimoradifaranam@gmail.com>
2024-09-01 11:33:25 +0330

    .

---
+++ JWTSampleProject/Migrations/20240901080154_initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTSampleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "Id");
+        }
+    }
+}
