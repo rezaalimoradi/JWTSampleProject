7d579c8b55370e2c4f2d5ad382fa25e9d3902e39
r.alimoradi <alimoradifaranam@gmail.com>
2024-09-02 21:57:22 +0330

    add multi middleware

---
+++ JWTSampleProject/Common/ConnectionUtility.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Common
+{
+    public interface IConnectionUtility
+    {
+        SqlConnection GetConfigDbConneciton();
+    }
+    public class ConnectionUtility : IConnectionUtility
+    {
+        private readonly Configs configs;
+        public ConnectionUtility(IOptions<Configs> options)
+        {
+            this.configs = options.Value;
+        }
+        public SqlConnection GetConfigDbConneciton()
+        {
+            return new SqlConnection(configs.DBConnection);
+        }
+
+
+    }
+}
