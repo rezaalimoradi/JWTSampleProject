e9e0eaa6687bd0c45e77ed3e449d68ee0e5602dc
r.alimoradi <alimoradifaranam@gmail.com>
2024-09-03 11:50:47 +0330

    changetypeiduserrole

--- JWTSampleProject/Migrations/20240903081907_changetypeiduserrole.Designer.cs
+++ JWTSampleProject/Migrations/20240903081907_changetypeiduserrole.Designer.cs
@@ -4,6 +4,7 @@ using JWTSampleProject.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JWTSampleProject.Migrations
 {
     [DbContext(typeof(SampleDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240903081907_changetypeiduserrole")]
+    partial class changetypeiduserrole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JWTSampleProject/Migrations/20240903081907_changetypeiduserrole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTSampleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class changetypeiduserrole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RoleId",
+                table: "UserRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "UserRoles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
